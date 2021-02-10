beede7ac4d46a403f40a77d3cdd5c21b45f6a08d
Dhruv Maniyaa <71778799+midnightmadwalk@users.noreply.github.com>
2021-02-10 16:49:23 +0530

    PeNdInG uPdAtEs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { initHandlers } from './handlers';
 
 (async () => {
     initHandlers();
-
+    await bot.telegram.deleteWebhook({drop_pending_updates: true});
     await bot.launch();
     console.log(`@${bot.botInfo?.username} is running...`);
 })();
